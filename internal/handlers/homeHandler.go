bcaf1d8a910aa50305158fd76fbcc2aa8de1d396
fl0rest <davortoth3@gmail.com>
2025-07-31 14:39:29 +0200

    updated frontend

--- internal/handlers/homeHandler.go
+++ internal/handlers/homeHandler.go
@@ -1,15 +1,27 @@
 package handlers
 
 import (
+	"ed-tracker/internal/db"
 	"ed-tracker/internal/logging"
 	"net/http"
+	"time"
 
 	_ "modernc.org/sqlite"
 )
 
+type IndexPageData struct {
+	LastUpdate string
+	Resources  []db.Resource
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log := logging.Log
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
 	resources, err := queries.ListResources(ctx)
 	if err != nil {
@@ -17,13 +29,18 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	var lastUpdate string
+	if len(resources) > 0 {
+		lastUpdate = time.Unix(resources[0].Time, 0).Format("2006-01-02 15:04:05")
+	}
+
+	data := IndexPageData{
+		LastUpdate: lastUpdate,
+		Resources:  resources,
 	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if err := indexTmpl.Execute(w, resources); err != nil {
+	if err := indexTmpl.Execute(w, data); err != nil {
 		log.Errorf("Error rendering template: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
